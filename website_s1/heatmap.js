995bd7150948e0262fc22fa95f5b6a537d32a719
szen95 <tstoh1@sheffield.ac.uk>
2019-03-23 13:17:14 +0000

    Resize logo in supplemental website.

--- website_s1/heatmap.js
+++ website_s1/heatmap.js
@@ -280,8 +280,8 @@ function heatmap_display(url, heatmapId, paletteName) {
             .attr("xlink:href", "website_s1/img/SEABED_logo.png")
             .attr("x", legendElementWidth * 4)
             .attr("y", legendPosTop / 15)
-            .attr("width", "20%")
-            .attr("height", "20%");
+            .attr("width", "40%")
+            .attr("height", "40%");
 
         legend.append("text")
             .attr("class", "mono legendElement")

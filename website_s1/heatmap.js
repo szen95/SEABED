a492ae0fddc857fcbb701928a05063630d58aa16
szen95 <tstoh1@sheffield.ac.uk>
2019-03-23 13:38:21 +0000

    Added SEABED logo.

--- website_s1/heatmap.js
+++ website_s1/heatmap.js
@@ -248,6 +248,14 @@ function heatmap_display(url, heatmapId, paletteName) {
             //   });
             
         }
+
+        var logo = svg.append("svg:image")
+            .attr("class", "logo")
+            .attr("xlink:href", "website_s1/img/SEABED_logo.png")
+            .attr("x", legendElementWidth * 4)
+            .attr("y", legendPosTop / 15)
+            .attr("width", "40%")
+            .attr("height", "40%");
         
         var legendElementText = [{ "label": "No differential response (i)", "value": 0 },
         { "label": "Sensitive to both MAPK and PI3K-AKT pathway inhibitors (ii)", "value": 1 },
